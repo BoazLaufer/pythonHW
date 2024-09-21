e7ad51e2304632c29ae200a1fabdd97d9551b42e
BoazLaufer <boazlaufer10@gmail.com>
2024-09-21 17:25:44 +0000

    with answer

--- Homeworks/hw_1/task1.py
+++ Homeworks/hw_1/task1.py
@@ -1,10 +1,10 @@
 num = 1
-num=int(input("enter a number"))
+num=int(input("enter a number - "))
 while num != 0:
     if (num%2 == 0):
         print ("The number is even")
     else:
         print ("The number is odd")
-    num=int(input("enter a number"))
+    num=int(input("enter a number - "))
     
 print ("Thank you")
