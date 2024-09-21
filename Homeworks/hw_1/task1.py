8422567c6a1423de1e08aad94de1d454416202f2
BoazLaufer <boazlaufer10@gmail.com>
2024-09-21 19:53:08 +0300

    Update task1.py
    after fix

--- Homeworks/hw_1/task1.py
+++ Homeworks/hw_1/task1.py
@@ -1,9 +1,12 @@
+num = 1
 num=int(input("enter a number"))
-if (num%2 ==0):
-  a=105
-while a>10:
-    a=a-5
-    print (a)
-print("bye  bye")
-if (num = 0):
-  print ("thank you")
+#while (num > 0) or (num < 0):
+while num != 0:
+    if (num%2 == 0):
+        print ("The number is even")
+    else:
+        print ("The number is odd")
+    num=int(input("enter a number"))
+    
+#if (num == 0):
+print ("Thank you")
